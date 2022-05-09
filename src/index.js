8da9d0dcfa12285eff7212a070cde21c0177bbd8
RichardPickman <drezzerock@gmail.com>
2022-05-09 21:56:18 +0200

    feat: add main code and template

--- src/index.js
+++ src/index.js
@@ -1,102 +1,86 @@
 import './sass/style.scss';
-import { createKeyboard } from './components/keyboard';
-import { createTextArea } from './components/textarea';
+import createElements from './components';
+import {
+  changeKeys,
+  changeColor,
+  changeLanguage,
+  textAreaHandler,
+  getLanguageFromStorage,
+} from './components/helpers';
 import * as layout from './layouts/index';
+import Keyboard from './classes/keyboard'
 
-const keys = {
-    shift: false,
-    caps: false,
+window.onload = function App() {
+  createElements();
+  const state = new Keyboard();
+  const { currentLanguage } = state;
+  getLanguageFromStorage(state);
+  const shortcuts = [];
+  let language = currentLanguage;
 
-}
+  const changeLayout = (event) => {
+    if (event === 'CapsLock' && state.caps === false) {
+      state.layout = layout[`${language}_CAPS`];
 
-window.onload = function () {
-    const root = document.createElement('main')
-    root.setAttribute('id', 'root')
-    document.body.append(root)
-    createTextArea()
-    createKeyboard()
+      changeKeys(state.layout);
+
+      state.caps = true;
+    } else if (event === 'CapsLock' && state.caps === true) {
+      state.layout = layout[`${language}`];
+
+      changeKeys(state.layout);
+
+      state.caps = false;
+    }
+
+    if (event === 'ShiftLeft' || event === 'ShiftRight') {
+      state.layout = layout[`${language}_SHIFT`];
+
+      changeKeys(state.layout);
+
+      state.shift = true;
+    }
+
+    textAreaHandler(event, state);
+  };
+
+  document.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    if (!layout.CODES.includes(event.code)) return;
+
+    changeLayout(event.code);
+    if (event.code === 'ControlLeft') shortcuts.push('ControlLeft');
+    if (event.code === 'AltLeft') shortcuts.push('AltLeft');
+
+    setTimeout(() => {
+      if (shortcuts.includes('ControlLeft') && shortcuts.includes('AltLeft')) {
+        language = changeLanguage(state, layout);
+      }
+
+      shortcuts.length = 0;
+    }, 300);
+  });
+
+  document.addEventListener('keyup', (event) => {
+    if (!layout.CODES.includes(event.code)) return;
     const keyboard = document.querySelectorAll('.key');
-    // CHANGE LAYOUTS 
-    document.addEventListener('keydown', function(event) {
-        
-        if (event.code == 'CapsLock' && keys.caps === false) {
-            keyboard.forEach((element, index) => {
-                element.textContent = layout.ENG_CAPS[index]
-            })
-            keys.caps = true
-        } else {
-            keyboard.forEach((element, index) => {
-                element.textContent = layout.ENG[index]
-            })
-            keys.caps = false
-        }
+    const index = Array.from(keyboard).findIndex((button) => button.id === event.code);
+    keyboard[index].classList.remove('setColor');
 
-        if (event.code == 'ShiftLeft' || event.code == 'ShiftRight') {
-            keys.shift = true;
-            keyboard.forEach((element, index) => {
-                element.textContent = layout.ENG_SHIFT[index]
-            })
-        }
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      state.layout = layout[`${language}`];
+      changeKeys(state.layout);
+      state.shift = false;
+    }
+  });
 
-        if (layout.ENG_SYMBOLS.includes(event.key) && textarea.textContent === null) {
-            const textarea = document.querySelector('#textarea')
-            textarea.textContent = event.key;
-        } else if (layout.ENG_SYMBOLS.includes(event.key)) {
-            let textAreaText = textarea.textContent + event.key
-    
-            textarea.textContent = textAreaText
-        }
+  document.querySelectorAll('.key').forEach((key) => {
+    key.addEventListener('click', (e) => {
+      const button = e.target.closest('button');
+      changeColor(button);
+      setTimeout(() => button.classList.remove('setColor'), 100);
+
+      textAreaHandler(button.id, state);
     });
-    
-    document.addEventListener('keyup', function(event) {
-        
-        if (event.code == 'ShiftLeft') {
-            keyboard.forEach((element, index) => {
-                element.textContent = layout.ENG[index]
-            })
-        }
-    });
-
-    keyboard.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const textarea = document.querySelector('#textarea')
-            if (layout.ENG_SYMBOLS.includes(e.target.textContent) && textarea.textContent === null) {
-                textarea.textContent = e.target.textContent;
-            } else if (layout.ENG_SYMBOLS.includes(e.target.textContent)) {
-                let textAreaText = textarea.textContent + e.target.textContent
-        
-                textarea.textContent = textAreaText
-            }
-
-            if (e.target.textContent == 'Caps' && keys.caps === false) {
-                keyboard.forEach((element, index) => {
-                    element.textContent = layout.ENG_CAPS[index];
-                })
-
-                keys.caps = true;
-            } else if (e.target.textContent == 'Caps' && keys.caps === true) {
-                keyboard.forEach((element, index) => {
-                    element.textContent = layout.ENG[index];
-                })
-
-                keys.caps = false;
-            }
-        })
-    })
-
-    const backspace = document.querySelector('#Backspace13')
-
-    backspace.addEventListener('click', () => {
-        const textarea = document.querySelector('#textarea')
-        if (textarea.textContent === null) {
-            textarea.textContent = '';
-        } else {
-            const textAreaText = [...textarea.textContent];
-
-            textAreaText.pop()
-
-            textarea.textContent = textAreaText.join('');
-        }
-    })
-    
-}
+  });
+};
