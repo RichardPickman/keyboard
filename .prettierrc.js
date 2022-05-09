51d0d9c7b8d2edeab96b7c4d1acf27245297cd5c
RichardPickman <drezzerock@gmail.com>
2022-05-09 21:57:47 +0200

    feat: remove everything except readme

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "tabWidth": 4,
-    "useTabs": false,
-    "trailingComma": "none",
-    "singleQuote": true,
-    "printWidth": 120,
-    "semi": true,
-    "arrowParens": "always"
-}
--- .stylelintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "extends": ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
-    "plugins": ["stylelint-scss"],
-  }
